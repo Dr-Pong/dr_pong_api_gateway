d61b249493b9bf2bf9a5e2cfd6b859bde650d810
NamJun Heo <90181905+nheo9143@users.noreply.github.com>
2023-05-18 10:56:14 +0900

    [Refactor] admin 제거

--- src/auth/type.user.roletype.ts
+++ src/auth/type.user.roletype.ts
@@ -1,6 +1,5 @@
 export const ROLETYPE_GUEST = 'guest' as const;
 export const ROLETYPE_NONAME = 'noname' as const;
 export const ROLETYPE_MEMBER = 'member' as const;
-export const ROLETYPE_ADMIN = 'admin' as const;
 
-export type RoleType = 'guest' | 'noname' | 'member' | 'admin';
+export type RoleType = 'guest' | 'noname' | 'member';
