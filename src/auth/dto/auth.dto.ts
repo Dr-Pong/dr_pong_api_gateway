cb02a943831043b98c30d1c4937c524046b7f57b
maindishes <65796820+maindishes@users.noreply.github.com>
2023-05-18 20:20:54 +0900

    [Fix] AuthDto 내부 생성자로 생성 수정

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,5 +1,19 @@
+import { RoleType } from '../type.user.roletype';
+
 export class AuthDto {
   id: number;
   nickname: string;
   secondAuthRequired: boolean;
+  roleType: RoleType;
+  constructor(
+    id: number,
+    nickname: string,
+    secondAuthRequired: boolean,
+    roleType: RoleType,
+  ) {
+    this.id = id;
+    this.nickname = nickname;
+    this.secondAuthRequired = secondAuthRequired;
+    this.roleType = roleType;
+  }
 }
