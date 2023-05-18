ef09ee2be9574f1ba230368f5f906ff4e9eb2bbf
maindishes <65796820+maindishes@users.noreply.github.com>
2023-05-18 20:24:21 +0900

    [Fix] eslint 적용 , findUser 함수 닉네임 확인후 roletype 결정

--- src/auth/jwt/auth.jwt.strategy.ts
+++ src/auth/jwt/auth.jwt.strategy.ts
@@ -1,52 +1,50 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common"
-import { PassportStrategy } from "@nestjs/passport"
-import { InjectRepository } from "@nestjs/typeorm"
-import { ExtractJwt, Strategy } from "passport-jwt"
-import { User } from "src/domain/user/user.entity"
-import { Repository } from "typeorm"
-import { AuthDto } from "../dto/auth.dto"
-import { TokenInterface } from "./jwt.token.interface"
-import { UserRepository } from "../user.repository"
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthDto } from '../dto/auth.dto';
+import { TokenInterface } from './jwt.token.interface';
+import { UserRepository } from '../user.repository';
+import { ROLETYPE_MEMBER, ROLETYPE_NONAME } from '../type.user.roletype';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor(
-		private readonly userRepository: UserRepository,
-	) {
-		super({
-			secretOrKey: 'jwtSecret',
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-		})
-	};
-	users: Map<string, AuthDto> = new Map();
+  constructor(private readonly userRepository: UserRepository) {
+    super({
+      secretOrKey: 'jwtSecret',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  users: Map<string, AuthDto> = new Map();
 
-	async validate(payload): Promise<AuthDto> {
-		const user = await this.findUser(payload);
-		this.validateUser(payload, user);
-		return user;
-	}
+  async validate(payload): Promise<AuthDto> {
+    const user = await this.findUser(payload);
+    this.validateUser(payload);
+    return user;
+  }
 
-	async findUser(token: TokenInterface): Promise<AuthDto> {
-		const userFromMemory = this.users.get(token.nickname);
+  async findUser(token: TokenInterface): Promise<AuthDto> {
+    const userFromMemory = this.users.get(token.nickname);
 
-		if (userFromMemory)
-			return userFromMemory;
+    if (userFromMemory) return userFromMemory;
 
-		const userFromDb = await this.userRepository.findByNickname(token['nickname']);
-		if (!userFromDb)
-			throw (new UnauthorizedException());
-		const existUser: AuthDto = {
-			id: userFromDb.id,
-			nickname: userFromDb.nickname,
-			roleType: userFromDb.roleType,
-			secondAuthRequired: token.secondAuthRequierd,
-		};
-		return existUser;
-	}
+    const userFromDb = await this.userRepository.findByNickname(
+      token['nickname'],
+    );
+    if (!userFromDb) throw new UnauthorizedException();
 
-	/** 닉네임 설정이 안되어 있거나 2차 인증이 필요하면 에러 반환 */
-	validateUser(token: TokenInterface, user: AuthDto): void {
-		if (token.nickname === null || token.secondAuthRequierd === true)
-			throw (new UnauthorizedException());
-	}
+    const existUser: AuthDto = new AuthDto(
+      userFromDb.id,
+      userFromDb.nickname,
+      token.secondAuthRequierd,
+      ROLETYPE_NONAME,
+    );
+    if (userFromDb.nickname) existUser.roleType = ROLETYPE_MEMBER;
+    return existUser;
+  }
+
+  /** 닉네임 설정이 안되어 있거나 2차 인증이 필요하면 에러 반환 */
+  validateUser(token: TokenInterface): void {
+    if (token.nickname === null || token.secondAuthRequierd === true)
+      throw new UnauthorizedException();
+  }
 }
