694ada9d07b7aa07d18b6a1208eaff09b9c9bc77
maindishes <65796820+maindishes@users.noreply.github.com>
2023-05-18 20:20:35 +0900

    [Chore] User import 경로 수정

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/domain/user/user.entity';
+import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { UpdateUserSignUpDto } from './dto/update.user.signup.dto';
 import { CreateUserDto } from './dto/create.user.dto';
