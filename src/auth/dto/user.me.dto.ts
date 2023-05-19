6985a811aebff75b8cafe55fcbf73a0343e8e078
maindishes <65796820+maindishes@users.noreply.github.com>
2023-05-19 13:19:29 +0900

    [Refactor] UserMeDto 생성자 안에 static 함수 추가

--- src/auth/dto/user.me.dto.ts
+++ src/auth/dto/user.me.dto.ts
@@ -1,4 +1,5 @@
 import { RoleType } from '../type.user.roletype';
+import { ROLETYPE_GUEST, ROLETYPE_NONAME } from '../type.user.roletype';
 
 export class UserMeDto {
   nickname: string;
@@ -15,5 +16,21 @@ export class UserMeDto {
     this.imgUrl = imgUrl;
     this.isSecondAuthOn = isSecondAuthOn;
     this.roleType = roleType;
+  }
+  static guestUserMe(): UserMeDto {
+    return {
+      nickname: '',
+      imgUrl: '',
+      isSecondAuthOn: false,
+      roleType: ROLETYPE_GUEST,
+    };
+  }
+  static nonameUserMe(): UserMeDto {
+    return {
+      nickname: '',
+      imgUrl: '',
+      isSecondAuthOn: false,
+      roleType: ROLETYPE_NONAME,
+    };
   }
 }
