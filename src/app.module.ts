76fdd906fc9aa843678caf674023e81bdd857458
maindishes <65796820+maindishes@users.noreply.github.com>
2023-05-24 15:40:35 +0900

    [Feat] typeORMConfig 설정 적용, 모듈 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatewayModule } from './gateway/gateway.module';
+import { TestModule } from './test/test.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.configs';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [GatewayModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return typeORMConfig;
+      },
+      async dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+        return addTransactionalDataSource({
+          dataSource: new DataSource(options),
+        });
+      },
+    }),
+    GatewayModule,
+    TestModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
