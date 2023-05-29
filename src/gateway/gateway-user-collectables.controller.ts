fc075222c44b60ab9aa66934b4fecaef1e69505c
maindishes <65796820+maindishes@users.noreply.github.com>
2023-05-29 20:43:30 +0900

    [Fix] controller 의 naming에서 get 제거  #20

--- src/gateway/gateway-user-collectables.controller.ts
+++ src/gateway/gateway-user-collectables.controller.ts
@@ -9,12 +9,22 @@ import {
   ParseBoolPipe,
   Logger,
   NotFoundException,
+  UseGuards,
+  UnauthorizedException,
 } from '@nestjs/common';
 import axios from 'axios';
 import { UserAchievementsResponseDto } from './dtos/user-achievements-response.dto';
 import { UserEmojisResponseDto } from './dtos/user-emoji-response.dto';
 import { UserTitlesResponseDto } from './dtos/user-titles-response.dto';
 import { ProfileImageDto } from './dtos/user-images-response.dto';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  PatchUserAchievementsRequestDto,
+  PatchUserEmojisRequestDto,
+  PatchUserImageRequestDto,
+  PatchUserMessageRequestDto,
+  PatchUserTitleRequestDto,
+} from './dtos/user-patch-input.dto';
 
 // 요청에 헤더도 포함해서 넘기기
 @Controller('/users')
@@ -102,70 +112,106 @@ export class UserGatewayCollectablesController {
   // ): Promise<UserGameRecordsResponseDto> {}
 
   // Patch
+  // TODO: Patch 인풋에도 Dto 로 Clgass validator 로 검증하기 -> 다를시 400 BadRequest 에러
+  @UseGuards(AuthGuard('jwt'))
   @Patch('/:nickname/title')
   async usersDetailByNicknamePatch(
     @Param('nickname') nickname: string,
-    @Body() id: number,
+    @Body() PatchRequestDto: PatchUserTitleRequestDto, // DTO 로 받기
   ): Promise<void> {
     try {
       await axios.patch(
         process.env.WEBSERVER_URI + `/users/${nickname}/title`,
         {
           data: {
-            id: id,
+            id: PatchRequestDto.id,
           },
         },
       );
-    } catch (error) {}
+    } catch (error) {
+      if (error.response?.status === 404) {
+        throw new NotFoundException(error.response.data.message);
+      } else throw error;
+    }
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch('/:nickname/image')
   async usersImageByNicknamePatch(
     @Param('nickname') nickname: string,
-    @Body() id: number,
+    @Body() PatchRequestDto: PatchUserImageRequestDto, // DTO 로 받기
   ): Promise<void> {
-    await axios.patch(process.env.WEBSERVER_URI + '/${nickname}/image', {
-      params: nickname,
-      data: {
-        id: id,
-      },
-    });
+    try {
+      await axios.patch(process.env.WEBSERVER_URI + '/${nickname}/image', {
+        params: nickname,
+        data: {
+          id: PatchRequestDto.id,
+        },
+      });
+    } catch (error) {
+      if (error.response?.status === 404) {
+        throw new NotFoundException(error.response.data.message);
+      } else throw error;
+    }
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch('/:nickname/message')
   async usersMessageByNicknamePatch(
     @Param('nickname') nickname: string,
-    @Body()
-    message: string,
+    @Body() PatchRequestDto: PatchUserMessageRequestDto, // DTO 로 받기
   ): Promise<void> {
-    await axios.patch(process.env.WEBSERVER_URI + '/${nickname}/message', {
-      params: nickname,
-      data: {
-        message: message,
-      },
-    });
+    try {
+      await axios.patch(process.env.WEBSERVER_URI + '/${nickname}/message', {
+        params: nickname,
+        data: {
+          message: PatchRequestDto.message,
+        },
+      });
+    } catch (error) {
+      if (error.response?.status === 404) {
+        throw new NotFoundException(error.response.data.message);
+      } else throw error;
+    }
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch('/:nickname/achievements')
   async userAchievementsByNicknamePatch(
     @Param('nickname') nickname: string,
-    @Body() ids: (number | null)[],
+    @Body() PatchRequestDto: PatchUserAchievementsRequestDto, // DTO로 받기
   ): Promise<void> {
-    await axios.patch(process.env.WEBSERVER_URI + '/${nickname}/achievements', {
-      params: nickname,
-      data: ids,
-    });
+    try {
+      await axios.patch(
+        process.env.WEBSERVER_URI + '/${nickname}/achievements',
+        {
+          params: nickname,
+          data: PatchRequestDto.ids,
+        },
+      );
+    } catch (error) {
+      if (error.response?.status === 404) {
+        throw new NotFoundException(error.response.data.message);
+      } else throw error;
+    }
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch('/:nickname/emojis')
   async userEmojisByNicknamePatch(
     @Param('nickname') nickname: string,
-    @Body()
-    ids: (number | null)[],
+    @Body() PatchRequestDto: PatchUserEmojisRequestDto, // DTO 로 받기
   ): Promise<void> {
-    await axios.patch(process.env.WEBSERVER_URI + '/${nickname}/emojis', {
-      params: nickname,
-      data: ids,
-    });
+    try {
+      await axios.patch(process.env.WEBSERVER_URI + `/${nickname}/emojis`, {
+        data: PatchRequestDto.ids,
+      });
+    } catch (error) {
+      if (error.response?.status === 404) {
+        throw new NotFoundException(error.response.data.message);
+      } else if (error.response?.status === 401) {
+        throw new UnauthorizedException(error.response.data.message);
+      } else throw error;
+    }
   }
 }
