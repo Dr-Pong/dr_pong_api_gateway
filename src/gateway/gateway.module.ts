7d4852c94a72b532744a94bc0425c922070686db
maindishes <65796820+maindishes@users.noreply.github.com>
2023-06-05 15:25:36 +0900

    [Feat] import RankController

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserGatewayCollectablesController } from './gateway-user-collectables.controller';
 import { UserGatewayDetailController } from './gateway-user-detail-controller.dto';
 import { UserGatewayStatRankController } from './gateway-user-stat-rank-controller';
+import { RankController } from './webserver/gateway-rank-controller';
 @Module({
   controllers: [
     UserGatewayCollectablesController,
     UserGatewayDetailController,
     UserGatewayStatRankController,
+    RankController,
   ],
 })
 export class GatewayModule {}
