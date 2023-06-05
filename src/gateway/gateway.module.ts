ace2d6d2c9f00e2b216aaac3842621a7b6e82523
maindishes <65796820+maindishes@users.noreply.github.com>
2023-06-05 13:14:45 +0900

    [Refactor] 파일 디렉토리 구조 변경
    - gateway module 에 GatewayRankController 모듈 import해주기

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserGatewayCollectablesController } from './webserver/gateway-user-collectables.controller';
 import { UserGatewayDetailController } from './webserver/gateway-user-detail-controller.dto';
 import { UserGatewayStatRankController } from './webserver/gateway-user-stat-rank-controller';
+import { GatewayRankController } from './webserver/gateway-rank-controller';
 @Module({
   controllers: [
     UserGatewayCollectablesController,
     UserGatewayDetailController,
     UserGatewayStatRankController,
+    GatewayRankController,
   ],
 })
 export class GatewayModule {}
