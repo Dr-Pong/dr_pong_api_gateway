7cfb08b96e6ea63adce1a42d9e971b107a0e950d
maindishes <65796820+maindishes@users.noreply.github.com>
2023-06-09 13:39:48 +0900

    [Feat] dto 에 validate pipe 데토레이터 적용

--- src/gateway/chatserver/dtos/friend-direct-message-new-response.dto.ts
+++ src/gateway/chatserver/dtos/friend-direct-message-new-response.dto.ts
@@ -1,3 +1,6 @@
+import { IsBoolean } from 'class-validator';
+
 export class FriendDirectMessageNewResponseDto {
+  @IsBoolean()
   hasNewChat: boolean;
 }
--- src/gateway/chatserver/dtos/friend-post-chat-request.dto.ts
+++ src/gateway/chatserver/dtos/friend-post-chat-request.dto.ts
@@ -1,3 +1,9 @@
+import { Transform } from 'class-transformer';
+import { IsString, Length } from 'class-validator';
+
 export class PostFriendChatRequestDto {
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  @Length(1, 100)
   message: string;
 }
