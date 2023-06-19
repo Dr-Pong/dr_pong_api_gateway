4457c52d35b4f5ee02ee21472e42eec0b2bdc9e4
maindishes <65796820+maindishes@users.noreply.github.com>
2023-06-19 18:51:05 +0900

    [Feat] winston  로거 사용 하기 #71
    - 아직 제대로 사용 하지 못하는것 같다.

--- src/gateway/webserver/gateway-rank-controller.ts
+++ src/gateway/webserver/gateway-rank-controller.ts
@@ -1,22 +1,34 @@
-import { Controller, Get, Logger, Query } from '@nestjs/common';
+import { Controller, Get, Inject, Logger, Query } from '@nestjs/common';
 import axios from 'axios';
 import { QueryValidatePipe } from 'src/gateway/validation/custom-query-validate-pipe';
 import { RanksBottomResponseDto } from './dtos/user-rank-bottom-respose.dto';
 import { RanksTopReponseDto } from './dtos/user-rank-response.dto';
+import { WINSTON_MODULE_NEST_PROVIDER, WinstonLogger } from 'nest-winston';
 
 @Controller('ranks')
 export class GatewayRankController {
   private readonly logger: Logger = new Logger(GatewayRankController.name);
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly webserverLogger: WinstonLogger,
+  ) {}
   @Get('/top')
   async rankTopGet(
     @Query('count', new QueryValidatePipe(3, 10)) count: number,
   ): Promise<RanksTopReponseDto> {
+    // this.logger.log('WEBSERVER', 'Log message');
+    this.webserverLogger.log('webserver_log', 'Log message');
     try {
       const response = await axios.get(
         process.env.WEBSERVER_URL + `/ranks/top?count=${count}`,
       );
       return response.data;
     } catch (error) {
+      this.webserverLogger.error(
+        'webserver_error',
+        JSON.stringify(error.response?.data),
+      );
+      this.logger.log(error.response?.data)
       throw error.response?.data;
     }
   }
