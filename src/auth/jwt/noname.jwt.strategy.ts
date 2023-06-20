1984bf0b711f9652e4d7222c88f37aa2b3e5549c
maindishes <65796820+maindishes@users.noreply.github.com>
2023-06-20 14:22:12 +0900

    [Feat] JwtStrategyNoname 가드 구현 #59

---
+++ src/auth/jwt/noname.jwt.strategy.ts
@@ -0,0 +1,36 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthDto } from '../dto/auth.dto';
+import { TokenInterface } from './jwt.token.interface';
+import { UserRepository } from '../../user/user.repository';
+import { ROLETYPE_NONAME } from '../../user/type.user.roletype';
+
+@Injectable()
+export class JwtStrategyNoname extends PassportStrategy(Strategy, 'jwtNoname') {
+  constructor(private readonly userRepository: UserRepository) {
+    super({
+      secretOrKey: 'jwtSecret',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  users: Map<string, AuthDto> = new Map();
+
+  async validate(payload): Promise<AuthDto> {
+    const user = await this.findUser(payload);
+    return user;
+  }
+
+  async findUser(token: TokenInterface): Promise<AuthDto> {
+    const userFromDb = await this.userRepository.findById(+token['id']);
+    if (userFromDb.nickname) throw new BadRequestException();
+
+    const existUser: AuthDto = new AuthDto(
+      userFromDb.id,
+      userFromDb.nickname,
+      token.secondAuthRequierd,
+      ROLETYPE_NONAME,
+    );
+    return existUser;
+  }
+}
