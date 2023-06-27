d2278aa9c59a284479a6163b23697e3adade581e
maindishes <65796820+maindishes@users.noreply.github.com>
2023-06-27 15:34:28 +0900

    [Chore] 정렬 #77

--- log/log.format.entry.dto.ts
+++ log/log.format.entry.dto.ts
@@ -4,10 +4,10 @@ export class ResponseLogEntryDto {
   level: string;
   message: string;
   type: string;
+  context: string;
   ip: string;
   ClassName: string;
   FunctionName: string;
-  context: string;
   userInfo: UserMeDto;
   return: any; // 일단 any 여기에 성공한 리턴값이 담길수도 있고, 실패한 리턴값이 담길수도 있음
 
@@ -24,10 +24,10 @@ export class RequestLogEntryDto {
   ip: string;
   ClassName: string;
   FunctionName: string;
+  userInfo: UserMeDto;
   originalUrl: string;
   method: string;
   requestInfo: any; // 이것도 dto로 만들어서 형식 다 맞춰도 됨
-  userInfo: UserMeDto;
 
   constructor(data: Partial<RequestLogEntryDto>) {
     Object.assign(this, data);
@@ -42,10 +42,10 @@ export class ErrorLogEntryDto {
   ip: string;
   ClassName: string;
   FunctionName: string;
+  userInfo: UserMeDto;
   errorMessage: string;
   stackTrace: string;
   error: any;
-  userInfo: UserMeDto;
 
   constructor(data: Partial<ErrorLogEntryDto>) {
     Object.assign(this, data);
