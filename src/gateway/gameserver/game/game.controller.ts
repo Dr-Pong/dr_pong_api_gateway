baf2b258a1b13ab31a8568afed1550b6e3c33aa5
maindishes <65796820+maindishes@users.noreply.github.com>
2023-07-17 15:57:08 +0900

    [Refactor] game api 초대 관련  chat server invitaion 폴더로 이동 #108

--- src/gateway/gameserver/game/game.controller.ts
+++ src/gateway/gameserver/game/game.controller.ts
@@ -12,98 +12,10 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import axios from 'axios';
 import { GameMode, GameType } from './type.game';
-import { GameInviteAcceptDto } from './dto/game-invite-accept.dto';
 
 @Controller('/games')
 export class GatewayGameController {
   private readonly logger: Logger = new Logger(GatewayGameController.name);
-
-  @Post('/invitation')
-  @UseGuards(AuthGuard('jwt'))
-  async gameInvitePost(
-    @Req() request,
-    @Param('nickname') nickname: string,
-    @Body() body: { nickname: string; mode: GameMode },
-  ): Promise<void> {
-    try {
-      const accessToken = request.headers.authorization;
-      const response = await axios.post(
-        process.env.GAMESERVER_URL + `/games/invitation`,
-        body,
-        {
-          headers: {
-            Authorization: accessToken,
-          },
-        },
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  @Delete('/invitation')
-  @UseGuards(AuthGuard('jwt'))
-  async gameInviteDelete(@Req() request): Promise<void> {
-    try {
-      const accessToken = request.headers.authorization;
-      const response = await axios.delete(
-        process.env.GAMESERVER_URL + `/games/invitation`,
-        {
-          headers: {
-            Authorization: accessToken,
-          },
-        },
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  @Patch('/invitation/:id')
-  @UseGuards(AuthGuard('jwt'))
-  async gameInviteAcceptPost(
-    @Req() request,
-    @Param('id') id: string,
-  ): Promise<GameInviteAcceptDto> {
-    try {
-      const accessToken = request.headers.authorization;
-      const response = await axios.patch(
-        process.env.GAMESERVER_URL + `/games/invitation/${id}`,
-        {
-          headers: {
-            Authorization: accessToken,
-          },
-        },
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  @Delete('/invitation/:id')
-  @UseGuards(AuthGuard('jwt'))
-  async gameInviteRejectDelete(
-    @Req() request,
-    @Param('id') id: string,
-  ): Promise<void> {
-    try {
-      const accessToken = request.headers.authorization;
-      const response = await axios.delete(
-        process.env.GAMESERVER_URL + `/games/invitation/${id}`,
-        {
-          headers: {
-            Authorization: accessToken,
-          },
-        },
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
 
   @Post('/queue/:type')
   @UseGuards(AuthGuard('jwt'))
