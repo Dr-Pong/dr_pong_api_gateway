409a55f18d8e0775c9edc639cd5fbd92615029e8
maindishes <manynolan1@gmail.com>
2023-07-31 10:42:36 +0900

    [Feat] postgres config apply env

--- src/configs/typeorm.configs.ts
+++ src/configs/typeorm.configs.ts
@@ -5,9 +5,9 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'api-db',
   port: 5432,
-  username: 'postgres', // postgres
-  password: 'postgres',
-  database: 'auth',
+  username: process.env.API_DB_USER,
+  password: process.env.API_DB_PASSWORD,
+  database: process.env.API_DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
   poolSize: 10,
